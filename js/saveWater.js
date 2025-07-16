1c1c4e368ea85361b0ad05622ae07568bee808f9
kilocharlie96 <cernak.kiko@gmail.com>
2025-07-16 18:03:24 +0200

    Implement countdown timer functionality and update UI elements

---
+++ js/saveWater.js
@@ -0,0 +1,7 @@
+    var waterAmount = 0.0;
+    
+    function saveWater() {
+        const consumedWater = Number(document.getElementById("consumed-water").value);
+        
+        console.log("Vypitá voda:", consumedWater, "ml");
+    }
